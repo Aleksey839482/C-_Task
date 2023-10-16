62d871f651fd6527430014e46c0e24a86cb7d749
Aleksey <hromm93@gmail.com>
2023-10-16 22:03:32 +0500

    Correction

--- Task3/Program.cs
+++ Task3/Program.cs
@@ -2,18 +2,18 @@
 //4 -> "-4, -3, -2, -1, 0, 1, 2, 3, 4"
 //2 -> " -2, -1, 0, 1, 2"
 
+Console.WriteLine("Введите целое число N: ");
+string strN = Console.ReadLine();
+int number = Convert.ToInt32(strN);
+for(int N = -number; N <= number; N++)
+{
+    Console.Write(N + " ");
+}
+
 //Задача №7. Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает последнюю цифру этого числа.
 //456 -> 6
 //782 -> 2
 //918 -> 8
-
-// Console.WriteLine("Введите целое число N: ");
-// string strN = Console.ReadLine();
-// int number = Convert.ToInt32(strN);
-// for(int N = -number; N <= number; N++)
-// {
-//     Console.Write(N + " ");
-// }
 
 Console.WriteLine("Введите трёхзначное целое число: ");
 int number = Convert.ToInt32(Console.ReadLine());
