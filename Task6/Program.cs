a1da0cab1d37d30eef0a779426388919f3bd370d
Aleksey <hromm93@gmail.com>
2023-10-19 18:31:59 +0500

    Task4, Task5, Task6

---
+++ Task6/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите Number1: ");
+string strNumber1 = Console.ReadLine();
+int number1 = Convert.ToInt32(strNumber1);
+Console.WriteLine("Введите Number2: ");
+string strNumber2 = Console.ReadLine();
+int number2 = Convert.ToInt32(strNumber2);
+if (number1 == number2 * number2 || number2 == number1 * number1)
+    {Console.WriteLine("Да");}
+else
+    {Console.WriteLine("Нет");}
---
+++ Task7/Program.cs
@@ -0,0 +1,36 @@
+﻿// Напишите программу, которая принимает на вход два числа и проверяет, является ли одно число квадратом другого.
+
+// 5, 25 -> да
+// -4, 16 -> да
+// 25, 5 -> да
+// 8,9 -> нет
+
+// Console.Write("Введите Number1: ");
+// int number1 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите Number2: ");
+// int number2 = Convert.ToInt32(Console.ReadLine());
+// int result = number1 % number2;
+// if (result == 0)
+// {
+//     Console.WriteLine("Первое число кратно второму");
+// }
+// else
+// {
+//     Console.WriteLine("Первое число не кратно второму, остаток: " + result);
+// }
+
+//Напишите программу, которая принимает на вход число и проверяет, кратно ли оно одновременно 7 и 23.
+// 14 -> нет
+// 46 -> нет
+// 161 -> да
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number % 7 == 0 && number % 23 == 0)
+{
+    Console.WriteLine("Да");
+}
+else
+{
+    Console.WriteLine("Нет");
+}
