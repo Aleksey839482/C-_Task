3dc8a54c6ddfc7f5dcf8bc6dae4bf88d853d1181
Aleksey839482 <hromm93@gmail.com>
2023-10-31 17:49:46 +0500

    Task9, Task10, Task11

---
+++ Task10/Program.cs
@@ -0,0 +1,45 @@
+﻿// Задайте массив из N случайных целых чисел (N вводится с
+// клавиатуры).
+// Найдите количество чисел, которые оканчиваются на 1 и
+// делятся нацело на 7.
+// Пример
+// [1 5 11 21 81 4 0 91 2 3]
+// => 2
+
+
+int[] GenerateArray (int arrayLength) 
+{
+    int[] array = new int[arrayLength];
+    for(int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(1,101);
+    }
+
+    return array;
+}
+void PrintArray (int[] array) 
+{
+    for(int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+}
+
+int CountNumbers (int[] array) 
+{
+    int number = 0;
+    for(int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 10 == 1 && array[i] % 7 == 0)
+        number++;
+    }
+    return number;
+}
+
+Console.Write("Введите длинну массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] array = GenerateArray(n);
+PrintArray(array);
+int number = CountNumbers(array);
+
+Console.WriteLine("Количество чисел: " + number);
---
+++ Task9/Program.cs
@@ -0,0 +1,58 @@
+﻿// Задайте одномерный массив, заполненый случайными числами.
+// Определите количество простых чисел в этом массиве.
+
+// Примеры
+
+// [1 3 4 19 3] => 2
+// [4 3 1 9 5 21 13] => 3
+
+// 1. Задать одномерный массив
+// 2. Определяем колчество простых чисел
+// 3. Вывод
+
+// Math.Pow();  // Степень
+// Math.Sqrt(); // Корень
+
+int[] GenerateArray () {
+    int[] array = new int[8];
+    for(int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(1,11);
+    }
+
+    return array;
+}
+void PrintArray (int[] array) {  // просто выводит массив
+    for(int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+}
+
+bool IsSimple (int number) {
+    for(int i = 2; i < number; i++)
+    {
+        if (number % i == 0 || number == 1)
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
+int CountSimpleNumber (int[] array){
+    int count = 0;
+    for(int i = 0; i < array.Length; i++)
+    {
+        if (IsSimple(array[i]))
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+int[] newArray = GenerateArray();
+PrintArray(newArray);
+int count = CountSimpleNumber(newArray);
+Console.WriteLine("Количство простых чисел: " + count);
